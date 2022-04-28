e44243f38160b6a647d213d71965a319cedd0aed
Emilia Bursell <jackbursell1@Sarahs-MacBook-Air.local>
2022-04-28 23:36:08 +0100

    text_adventure

---
+++ Fire Emblem.py
@@ -0,0 +1,2 @@
+#the purpose of this program is to serve as a more user-friendly interface to update the database on MySQl, while also
+#reinforcing my understanding of how Python integrates with MySQL
