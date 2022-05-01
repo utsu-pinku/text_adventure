52eecb7892c844a2ed6d9f154be42cb3b7ca634f
sammypanda <38725335+sammypanda@users.noreply.github.com>
2022-05-01 21:43:20 +1000

    Implemented data-driven death message

--- Text_adventure.py
+++ Text_adventure.py
@@ -4,7 +4,6 @@ def main():
     with open("database.json", "r") as read_file: # open an external file and put into 'read_file' variable
         database = json.load(read_file) # pass the raw json data into python so we can use it properly
 
-    death= "\n \n YOU ARE DEAD"
     error = "I'm sorry, I didn't catch that. Please select a path by choosing the appropriate letter."
     def branch(branch_num): # create a reusable function for branches
         db_object = database["branches"][branch_num - 1] # find the branch in the branches array that we want to interact with
@@ -17,6 +16,8 @@ def main():
             if selection.upper() == db_object["correct_branch"]: # if the selection is the correct branch as defined in the database
                 # return True
                 branch(branch_num + 1) # move on to the next branch (inconsiderate to whether one actually exists)
+            else:
+                print(database["death"])
         else: # if the users selection is NOT a valid option from the database
             print(error + "\n") # print the pre-defined error msg
             main() # restart back to the top
